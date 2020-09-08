1c73371069ef5be461a8f7d966ce602ffd24fbc0
Pedro Roccon <pedro@pedroroccon.com.br>
2020-09-08 11:35:27 -0300

    Using Mockery to test classes

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -2,9 +2,13 @@
 
 namespace Tests\Unit;
 
+use Mockery;
 use App\Retailer;
 use App\Stock;
+use Facades\App\Clients\ClientFactory;
+use App\Clients\Client;
 use App\Clients\ClientException;
+use App\Clients\StockStatus;
 use Tests\TestCase;
 use RetailerWithProductSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -27,5 +31,27 @@ class StockTest extends TestCase
 		// doesn't have a client class 
 		// Then an exception should be thrown.
 		Stock::first()->track();
+	}
+
+	/** @test */
+	function it_updates_local_stock_status_after_being_tracked()
+	{
+		$this->seed(RetailerWithProductSeeder::class);
+
+		// At this point we have 
+		// the ClientFactory to determine the appropriate Client.
+		// to use the checkAvailability() method.
+		
+		// We are using Mockery to mock the ClientFactory and expect a call 
+		// to make() method. Then, we get the result of the call, and 
+		// mock the return again to call the checkAvailability() method.
+
+		// You can add variables to the arguments, so after 6 months 
+		// you'll remember what each argument is :)
+		ClientFactory::shouldReceive('make->checkAvailability')->andReturn(new StockStatus($available = true, $price = 9900));
+
+		$stock = tap(Stock::first())->track();
+		$this->assertTrue($stock->in_stock);
+		$this->assertEquals(9900, $stock->price);
 	}
 }
