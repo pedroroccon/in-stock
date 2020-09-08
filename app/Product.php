bcd349749e1d86acfca4a1991eb5d800d9185bcb
Pedro Roccon <pedro@pedroroccon.com.br>
2020-09-08 08:22:45 -0300

    First tests and new models

---
+++ app/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+    	'name', 
+    ];
+
+    public function stock()
+    {
+    	return $this->hasMany(Stock::class);
+    }
+
+    public function inStock()
+    {
+        // You can use magic methods too: return $this->stock()->whereInStock(true)->exists();
+    	return $this->stock()->where('in_stock', true)->exists();
+    }
+
+
+}
--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,33 @@
 
 namespace Tests\Feature;
 
+use App\Product;
+use App\Retailer;
+use App\Stock;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function it_checks_stock_for_products_at_retailers()
+    {
+        $switch = Product::create(['name' => 'Nintendo Switch']);
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+
+        $this->assertFalse($switch->inStock());
+
+        $stock = new Stock([
+            'price' => 1000, 
+            'url' => 'http://foo.bar', 
+            'sku' => '45684', 
+            'in_stock' => true
+        ]);
+
+        $bestBuy->addStock($switch, $stock);
+
+        $this->assertTrue($switch->inStock());
     }
 }
